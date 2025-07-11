945f5b6a97a2621274b16440c03b392c66d86802
phamduc2009 <pduc9471@gmail.com>
2025-07-11 09:18:51 +0700

    Add files via upload

---
+++ Tree Algorithms/DistanceQueries.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define freopen(name) if(fopen(name".INP","r")) {freopen (name".INP","r",stdin); freopen (name".OUT","w",stdout);}
+#define fir first
+#define sec second
+#define str string
+const int Max_n=2*1e5;
+const ll mod=1e9+7;
+vector <int> g[Max_n+3];
+int h[Max_n+3], up[Max_n+3][20];
+void dfs(int u, int par){
+    for (auto v : g[u]){
+         if(v == par) continue;
+         h[v] = h[u] + 1;
+         up[v][0] = u;
+         for (int j = 1; j <= 19; ++j){
+              up[v][j] = up[up[v][j-1]][j-1];
+         }
+         dfs(v, u);
+    }
+}
+int lca(int u, int v){
+    if(h[u] < h[v]) swap(u, v);
+    int k = h[u] - h[v];
+    for (int j = 19; j >= 0; --j){
+         if(k >> j & 1){
+            u = up[u][j];
+         }
+    }
+    if(u == v) return u;
+    k = __lg(h[u]);
+    for (int j = k; j >= 0; --j){
+         if(up[u][j] != up[v][j]){
+            u = up[u][j];
+            v = up[v][j];
+         }
+    }
+    return up[u][0];
+}
+int main(){
+    freopen("");
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    int n, q; cin >> n >> q;
+    for (int i = 1; i <= n - 1; ++i){
+    	 int u, v; cin >> u >> v;
+         g[u].push_back(v);
+         g[v].push_back(u);
+    }
+    dfs(1, -1);
+    while (q--){
+        int u, v; cin >> u >> v;
+        int x = lca(u, v);
+        cout << h[u] - h[x] + h[v] - h[x] << '\n';
+    }
+    return 0;
+}//111111111111111111111111
