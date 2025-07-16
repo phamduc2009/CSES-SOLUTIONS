d78599a95ced72f7b2a58ff804d1009e636e2fda
phamduc2009 <pduc9471@gmail.com>
2025-07-16 09:27:03 +0700

    Add files via upload

---
+++ Tree Algorithms/PathQueries.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define freopen(name) if(fopen(name".INP","r")) {freopen (name".INP","r",stdin); freopen (name".OUT","w",stdout);}
+#define fir first
+#define sec second
+#define str string
+const int Max_n=2*1e5;
+const ll mod=1e9+7;
+ll b[Max_n+3], st[8*Max_n+3], in[2*Max_n+3], out[2*Max_n+3], timer, a[2*Max_n+3];
+vector <int> g[Max_n+3];
+void build(int id, int l, int r){
+    if(l == r){
+       st[id] = a[l];
+       return;
+    }
+    int m = (l + r) / 2;
+    build(2 * id, l, m);
+    build(2 * id + 1, m + 1, r);
+    st[id] = st[2 * id] + st[2 * id + 1];
+}
+void update(int id, int l, int r, int pos, ll val){
+    if(l == r){
+       st[id] = val;
+       return;
+    }
+    int m = (l + r) / 2;
+    if(pos <= m) update(2 * id, l, m, pos, val);
+    else update(2 * id + 1, m + 1, r, pos, val);
+    st[id] = st[2 * id] + st[2 * id + 1];
+}
+ll get(int id, int l, int r, int u, int v){
+    if(u > r || v < l) return 0;
+    if(u <= l && r <= v) return st[id];
+    int m = (l + r) / 2;
+    return get(2 * id, l, m, u, v) + get(2 * id + 1, m + 1, r, u, v);
+}
+void dfs(int u, int par){
+    in[u] = ++timer;
+    for (auto v : g[u]){
+         if(v == par) continue;
+         dfs(v, u);
+    }
+    out[u] = ++timer;
+}
+int main(){
+    freopen("");
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    int n, q; cin >> n >> q;
+    for (int i = 1; i <= n; ++i) cin >> b[i];
+    for (int i = 1; i <= n - 1; ++i){
+         int u, v; cin >> u >> v;
+         g[u].push_back(v);
+         g[v].push_back(u);
+    }
+    dfs(1, -1);
+    for (int u = 1; u <= n; ++u) a[in[u]] = b[u], a[out[u]] = -b[u];
+    build(1, 1, timer);
+    while (q--){
+        int k, u; cin >> k >> u;
+        if(k == 1){
+           int x; cin >> x;
+           update(1, 1, timer, in[u], x);
+           update(1, 1, timer, out[u], -x);
+           a[u] = x;
+        }
+        else cout << get(1, 1, timer, 1, in[u]) << '\n';
+    }
+    return 0;
+}//111111111111111111111111
