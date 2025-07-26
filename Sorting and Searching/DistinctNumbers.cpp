1ea838880ae857e6869767dec07f94b10410cdb1
phamduc2009 <pduc9471@gmail.com>
2025-07-26 22:07:52 +0700

    Add files via upload

---
+++ Sorting and Searching/DistinctNumbers.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define freopen(name) if(fopen(name".INP","r")) {freopen (name".INP","r",stdin); freopen (name".OUT","w",stdout);}
+#define fir first
+#define sec second
+#define str string
+const int Max_n=2*1e5;
+const ll mod=1e9+7;
+int main(){
+    freopen("");
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    int n; cin >> n;
+    set <int> s;
+    for (int i = 1; i <= n; ++i){
+    	 int x; cin >> x;
+    	 s.insert(x);
+    }
+    cout << s.size();
+    return 0;
+}//111111111111111111111111
