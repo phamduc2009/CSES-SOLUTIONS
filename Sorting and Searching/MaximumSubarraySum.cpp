3e73fc607740fc40a24b2bd5f040415b3f573872
phamduc2009 <pduc9471@gmail.com>
2025-07-27 21:04:19 +0700

    Add files via upload

---
+++ Sorting and Searching/MaximumSubarraySum.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define freopen(name) if(fopen(name".INP","r")) {freopen (name".INP","r",stdin); freopen (name".OUT","w",stdout);}
+#define fir first
+#define sec second
+#define str string
+const int Max_n=2*1e5;
+const ll mod=1e9+7;
+int a[Max_n+3];
+ll res = -1e18, sum = 0;
+int main(){
+    freopen("");
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    int n; cin >> n;
+    for (int i = 1; i <= n; ++i) cin >> a[i];
+    for (int i = 1; i <= n; ++i){
+         sum += a[i];
+         res = max(res, sum);
+         if(sum < 0) sum = 0;
+    }
+    cout << res;
+    return 0;
+}//111111111111111111111111
